2004344bb4c3fb703eb361faad86e233c69e8f7e
Bryan <boviedo4550@uta.edu.ec>
2022-06-12 23:11:38 -0500

    HOLA

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -17,6 +17,7 @@ public class Practica {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Edison Sanchez");
+        System.out.println("El de arriba es gay");
     }
     
 }
